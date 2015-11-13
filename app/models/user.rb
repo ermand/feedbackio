ee0a2f8637764ecdff99562bf3269960a25322f4
Ermand Durro <ermand.durro@helpling.com>
2015-11-13 18:08:11 +0100

    Add validation for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :async
+
+  before_save :downcase_email
+
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :encrypted_password, length: { minimum: 6 }, allow_blank: true
+
+  private
+    # Converts email to all lower-case
+    def downcase_email
+      self.email.downcase!
+    end
 end
