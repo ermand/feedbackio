b52a8f5b1c29a8b92f3b1d182cbe8a498322b78b
Ermand Duro <ermand.duro@gmail.com>
2015-11-17 15:43:57 +0100

    Add undo for company create as well.

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,4 @@ class Company < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 30 },
       uniqueness: { case_sensitive: false }
-  validates :state, presence: true, length: { maximum: 30 }
 end
